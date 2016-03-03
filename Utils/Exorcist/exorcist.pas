637e3772c88639f1cc3ebde8f106e4b6ef7f6e31
code_glitch <bgreggsmith@gmail.com>
2016-03-03 22:45:07 +0000

    UCAN-Utils/Exorcist: 1.0 Update
    1.0 Update.
    Added tty list menu
    Added menu based configuration
    Added help top bar
    Added real time pause/resume support
    Improved boot sequence

--- Utils/Exorcist/exorcist.pas
+++ Utils/Exorcist/exorcist.pas
@@ -17,6 +17,8 @@ type
 			DBGID: array of ANSIString;
 			DLength: Int64;
 		end;
+	ArANSIString = Array of ANSIString;
+	pArANSIString = ^ArANSIString;
 
 var
 	Link: ArduinoConnection;
@@ -25,6 +27,7 @@ var
 	CmdIn: Command;
 	DBGCodeDB: DBGCodeList;
 	StartTime: Int64;
+	k: Char;
 
 function TabStrip(Data: ANSIString): ANSIString;
 var
@@ -99,7 +102,7 @@ begin
 	
 	Tgt();
 	
-	GoToXY(50, c);
+	GoToXY(70, c);
 	writeln('[done!]');
 end;
 
@@ -113,7 +116,7 @@ begin
 	
 	Tgt();
 	
-	GoToXY(50, c);
+	GoToXY(70, c);
 	writeln('[done!]');
 end;
 
@@ -147,31 +150,244 @@ begin
 		TextColor(Red);
 end;
 
+
+function YNPrompt(Item: ANSIString): Boolean;
+var
+	c, lc: Char;
+	ConditionMet: Boolean = False;
+	
+begin
+	if WhereX() > 1 then
+		GoToXY(1, WhereY());
+		
+	TextBackGround(Red);
+	ClrEol();
+	
+	c := 'n';
+	
+	write(Item);
+	
+	GoToXY(40, WhereY());
+	
+	write('[y/N]');
+	TextBackGround(Black);
+	ClrEol();
+	
+	GoToXY(50, WhereY());
+	repeat
+		lc := c;
+		c := ReadKey();
+		if (ord(c) <> 13) and (ord(c) <> 8) then
+			write(c);
+			
+		if ((c = 'y') or (c = 'n'))then
+			ConditionMet := True;
+			
+		until ConditionMet = True;
+	
+	GoToXY(1, WhereY());
+		
+	TextBackGround(Black);
+	ClrEol();
+	
+	write(Item);
+	
+	GoToXY(40, WhereY());
+	
+	if c = 'y' then
+		TextBackGround(Green)
+	else
+		TextBackGround(Red);
+		
+	write(lc);
+	TextBackGround(Black);
+	
+	writeln('');
+	
+	if c = 'y' then
+		YNPrompt := True
+	else
+		YNPrompt := False;
+end;
+
+procedure DrawStrList(pBuf: pArANSIString; Sz: Int64);
+var
+	c: Int64;
+
+begin
+	c := 0;
+	repeat
+		c += 1;
+		writeln(' ',c,'	 - ',pBuf^[c]);
+		until c >= Sz;
+end;
+
+function PortList(pBuf: pArANSIString): Int64;
+var
+	c, n: Int64;
+	
+begin	
+	c := 0;
+	n := 0;
+	repeat
+		if DoesFileExist('/dev/ttyUSB' + IntToStr(c)) then
+			begin
+				n += 1;
+				pBuf^[n] := '/dev/ttyUSB' + IntToStr(c);
+			end;
+		c += 1;
+		until c >= 10;
+		
+	c := 0;
+	repeat
+		if DoesFileExist('/dev/ttyACM' + IntToStr(c)) then
+			begin
+				n += 1;
+				pBuf^[n] := '/dev/ttyACM' + IntToStr(c);
+			end;
+		c += 1;
+		until c >= 10;
+	
+	PortList := n;
+end;
+
+procedure ConnectionMenu();
+var
+	c, Sel: Int64;
+	ConPort: array of ANSIString;
+	ConPorts: Int64;
+	LineIn: ANSIString;
+	
 begin
 	ClrScr();
-	writeln('UCAN Debug Helper - Exorcist');
+	writeln('Connection port:');
+	writeln('       (Hit [`] to refresh');
+	writeln('');
+	
+	SetLength(ConPort, 21);
+	ConPorts := PortList(@ConPort);
+	
+	DrawStrList(@ConPort, ConPorts);
+	
+	Sel := 1;
+	repeat	
+		c := 0;
+		repeat
+			GoToXY(1, c + 3);
+			write(' ');
+			c += 1;
+			until c >= 21;
+			
+		GoToXY(1, Sel + 3);
+		write('*');
+		
+		k := ReadKey();
+		if ord(k) = 13 then
+			TgtPort := ConPort[Sel];
+		if k = '`' then
+			begin
+				ConPorts := PortList(@ConPort);
+				
+				ClrScr();
+				writeln('Connection port:');
+				writeln('       (Hit [`] to refresh');
+				writeln('');
+				
+				DrawStrList(@ConPort, ConPorts);
+			end;
+			
+		if (ord(k) = 72) and (Sel > 1) then
+			Sel -= 1;
+		if (ord(k) = 80) and (Sel < ConPorts) then
+			Sel += 1;
+			
+		until ord(k) = 13;
+	
+	ClrScr();
+end;
+
+procedure ConfMenu();
+var
+	Baud, Timeout, Sel: Int64;
+	SubSel: array [1..2] of Int64;
+	
+begin
+	ClrScr();
+	writeln('Connecting to ',TgtPort,' with:');
+	writeln('     Bits    : 8');
+	writeln('     Baud    : 9600');
+	writeln('     Timeout : 2000');
+	
+	Baud := 9600;
+
+	Sel := 4;
+	
+	Timeout := 2000;
+	
+	repeat
+		ClrScr();
+		writeln('Configure: ');
+		writeln('   Bits      : 8');
+		writeln('   Baud      : ', Baud);
+		writeln('   Timeout   : ', TimeOut);
+		writeln('   Accept');
+		
+		GoToXY(1, Sel + 1);
+		if Sel < 4 then
+			write('<>')
+		else
+			write('*');
+		
+		k := ReadKey();
+		if (ord(k) = 72) and (Sel > 1) then
+			Sel -= 1
+		else if (ord(k) = 80) and (Sel < 4) then
+			Sel += 1;
+		
+		if (ord(k) = 75) and (Sel = 2) and (Baud > 9600) then
+			Baud -= 9600
+		else if (ord(k) = 77) and (Sel = 2) and (Baud < 115200) then
+			Baud += 9600;
+			
+		if (ord(k) = 75) and (Sel = 3) and (Timeout > 100) then
+			Timeout -= 100
+		else if (ord(k) = 77) and (Sel = 3) then
+			Timeout += 100;
+		
+		until ord(k) = 13;
+	
+	Link.BaudRate := Baud;
+	Link.LineTimeout := Timeout;
+end;
+
+var
+	wy: Int64;
+
+begin
+	ClrScr();
+	writeln('*-----------------------------------------------------------------------------*');
+	writeln('                       UCAN Debug Helper - Exorcist 1.0');
+	writeln('');
 	
 	ObjInit('Initialize ISCE', @Link.Initialize);
 	CorInit('Initialize debug aliases from ucandebug.h', @Init_DBGAlias);
 	
 	writeln('Exorcist INIT complete.');
+	writeln('*-----------------------------------------------------------------------------*');
 	writeln('Hit <ESC> to exit.');
 	writeln('');
+	Pause(1000);
 	
-	write('Connection port:');
-	readln(TgtPort);
+	ConnectionMenu();
+	ConfMenu();
+	
+	writeln('Connection port:', TgtPort);
+	
 	CorInit('Clearing relevant lock files', @Init_Unlock);
-
-	writeln('Connecting to ',TgtPort,' with:');
-	writeln('	bits    : 8');
-	writeln('	baud    : 9600');
-	writeln('	Timeout : 2000');
-	with Link do
-		begin
-			BaudRate := 9600;
-			LineTimeout := 2000;
-			BitLength := 8;
-		end;
+	
+	Pause(1000);
+	
+	Link.BitLength := 8;
 	Link.Connect(TgtPort);
 	writeln('Connected!');
 	writeln('');
@@ -182,7 +398,7 @@ begin
 		if Length(LineIn) > 0 then
 			begin
 				CmdIn := ParseCommandFromString(LineIn);
-				CmdIn := PadCommand(CmdIn, 10);
+				CmdIn := PadCommand(CmdIn, 20);
 				CmdIn.Data[2] := ResolveDBGCode(CmdIn.Data[2]);
 				if CmdIn.Data[2] = 'UCAN_Debug_Boot' then
 					begin
@@ -194,12 +410,30 @@ begin
 				Colourizer(CmdIn.GetAsHumanString());
 				writeln(GetTime() - StartTime,': ',CmdIn.GetAsHumanString());
 				TextColor(Lightgray);
+				
+				wy := WhereY();
+				GoToXY(1, 1);
+				TextBackGround(Blue);
+				ClrEol();
+				write(TgtPort + ': Hit <ESC> to exit, <SPACE> to pause');
+				TextBackGround(Black);
+				GoToXY(1, wy);
 			end;
 		
 		if Keypressed() then
 			begin
-				if ReadKey() = #27 then
+				k := ReadKey();
+				if k = ' ' then
+					begin
+						while Keypressed() = false do
+							Pause(1);
+						k := ReadKey();
+					end;
+					
+				if k = #27 then
 					Break;
+					
+				k := #0;
 			end;
 		
 		until 9-8=7;
